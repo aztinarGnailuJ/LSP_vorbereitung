4b3b6632af89e3cd74136e5f0c2321348a4042b9
aztinarGnailuJ <jgranitza@gmx.de>
2022-02-16 16:08:43 +0100

    corrected 2a

--- klausur 20/aufgabe 2a.pl
+++ klausur 20/aufgabe 2a.pl
@@ -1,6 +1,6 @@
-membr(A,[]) :- fail.
-membr(A, [A|L]):- !.
-membr(A,[E|L]) :- membr(A, L).
+membr(_,[]) :- fail.
+membr(A, [A|_]):- !.
+membr(A,[_|L]) :- membr(A, L).
 
 dupl_search(_, [], []).
 dupl_search(E,[E|L],Lw) :-
@@ -9,10 +9,10 @@ dupl_search(E, [A|L], [A|Lw]) :-
     dupl_search(E,L,Lw).
 
 find_duplicates(L, D) :-
-    find_duplicates(L, D), not(De = []).
+    find_duplicates_aux(L, D), not(D = []).
 
-find_duplicates_aux([E], []).
-find_duplicates_aux([E|L], [E|D]) :- 
-    membr(E, L), dupl_search(E, L, Lwoe), find_duplicates(Lwoe, D), !.
-find_duplicates_aux([E|L], D) :- 
-    find_duplicates(L, D).
+find_duplicates_aux([], []).
+find_duplicates_aux([E|L], [E|D]) :-
+    member(E,L), !, dupl_search(E, L, Lwoe), find_duplicates_aux(Lwoe, D).
+find_duplicates_aux([E|L], D) :-
+    not(member(E,L)), find_duplicates_aux(L,D).
